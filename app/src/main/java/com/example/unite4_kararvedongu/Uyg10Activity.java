d5c84851b987735d73384ecfb451a2afa3d163e0
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-23 11:59:21 +0300

    2312.2022 saat 11.57

--- app/src/main/java/com/example/unite4_kararvedongu/Uyg10Activity.java
+++ app/src/main/java/com/example/unite4_kararvedongu/Uyg10Activity.java
@@ -1,13 +1,37 @@
 package com.example.unite4_kararvedongu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Uyg10Activity extends AppCompatActivity {
+    EditText txtFaktoriyel;
+    TextView viewSonuc;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg10_layout);
+
+        txtFaktoriyel = findViewById(R.id.txtfak);
+        viewSonuc = findViewById(R.id.viewSonuc);
+
+    }
+
+    public void btnhesapla(View view) {
+        int faktoriyel = Integer.parseInt(txtFaktoriyel.getText().toString());
+        int sayac = 1;
+        long sonuc = 1;
+        while (sayac <= faktoriyel) {
+            sonuc = sonuc * sayac;
+            sayac++;
+        }
+        viewSonuc.setText("sonuc:"+sonuc);
     }
 }
+
